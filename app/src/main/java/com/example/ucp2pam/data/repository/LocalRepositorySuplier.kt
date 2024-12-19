47c59304062d59fd7db95f0ab9f8c8403a90ee8f
agilimam <agilimamalhafizi04@gmail.com>
2024-12-19 07:08:44 +0700

    membuat operasi untuk suplierdao

---
+++ app/src/main/java/com/example/ucp2pam/data/repository/LocalRepositorySuplier.kt
@@ -0,0 +1,18 @@
+package com.example.ucp2pam.data.repository
+
+import com.example.ucp2pam.data.dao.SuplierDao
+import com.example.ucp2pam.data.entity.Suplier
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositorySuplier (
+    private val suplierDao: SuplierDao
+) : RepositorySuplier {
+    override suspend fun insertSuplier(suplier: Suplier) {
+        suplierDao.insertSuplier(suplier)
+    }
+
+    override fun getAllSuplier(): Flow<List<Suplier>> {
+        return suplierDao.getAllSuplier()
+    }
+
+}
