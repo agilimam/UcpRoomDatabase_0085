b838ced7521c1a3721dbadb3bb3a02554146ac73
agilimam <agilimamalhafizi04@gmail.com>
2024-12-22 08:56:35 +0700

    Menambahkan ekstensi fungsi toBarangEntity untuk mengonversi BarangEvent menjadi entity Barang

--- app/src/main/java/com/example/ucp2pam/ui/viewmodel/Barang/InsertBrgViewModel.kt
+++ app/src/main/java/com/example/ucp2pam/ui/viewmodel/Barang/InsertBrgViewModel.kt
@@ -2,6 +2,14 @@ package com.example.ucp2pam.ui.viewmodel.Barang
 
 import com.example.ucp2pam.data.entity.Barang
 
+fun BarangEvent.toBarangEntity():Barang = Barang(
+    id = id,
+    Nama = Nama,
+    Deskripsi = Deskripsi,
+    Harga = Harga,
+    Stok = Stok,
+    NamaSuplier = NamaSuplier
+)
 data class BarangEvent(
     val id: String = "",
     val Nama: String = "",
