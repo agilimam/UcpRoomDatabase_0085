1ea9faa46bd189093ef569dbae10c26c06260159
agilimam <agilimamalhafizi04@gmail.com>
2024-12-22 08:48:04 +0700

    Menambahkan tampilan HomeBrgView dengan Scaffold, TopAppBar, dan FloatingActionButton untuk menampilkan daftar barang

--- app/src/main/java/com/example/ucp2pam/ui/view/Barang/HomeBrgView.kt
+++ app/src/main/java/com/example/ucp2pam/ui/view/Barang/HomeBrgView.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
@@ -17,11 +18,16 @@ import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
@@ -29,16 +35,57 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2pam.data.entity.Barang
+import com.example.ucp2pam.ui.costumwidget.TopAppBar
 import com.example.ucp2pam.ui.viewmodel.Barang.HomeBrgUiState
+import com.example.ucp2pam.ui.viewmodel.Barang.HomeBrgViewModel
+import com.example.ucp2pam.ui.viewmodel.PenyediaViewModel
 import kotlinx.coroutines.launch
 
 @Composable
 fun HomeBrgView(
+    viewModel: HomeBrgViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onAddBrg: () -> Unit = { },
+    onDetailClik: (String) -> Unit = { },
+    modifier: Modifier = Modifier
 
 ){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                judul = "Daftar Barang",
+                showBackButton = false,
+                onBack = { },
 
+                )
+        },
+
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddBrg,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Barang",
+                )
+            }
+        }
+    ) { innerPadding ->
+        val homeBrgUiState by viewModel.homeBrgUiState.collectAsState()
+
+        BodyHomeBrgView(
+            homeBrgUiState = homeBrgUiState,
+            onClick = {
+                onDetailClik(it)
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
 }
+
 @Composable
 fun BodyHomeBrgView(
     homeBrgUiState: HomeBrgUiState,
