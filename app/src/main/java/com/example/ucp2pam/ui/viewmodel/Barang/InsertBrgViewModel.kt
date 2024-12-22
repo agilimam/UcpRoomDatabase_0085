975ac1e6617a5e91a9b9631bf009f90c54ec26ea
agilimam <agilimamalhafizi04@gmail.com>
2024-12-22 08:54:06 +0700

    membuat sebuah data clas untuk barangEvent

---
+++ app/src/main/java/com/example/ucp2pam/ui/viewmodel/Barang/InsertBrgViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.ucp2pam.ui.viewmodel.Barang
+
+import com.example.ucp2pam.data.entity.Barang
+
+data class BarangEvent(
+    val id: String = "",
+    val Nama: String = "",
+    val Deskripsi: String = "",
+    val Harga: String = "",
+    val Stok: String = "",
+    val NamaSuplier: String = "",
+)
