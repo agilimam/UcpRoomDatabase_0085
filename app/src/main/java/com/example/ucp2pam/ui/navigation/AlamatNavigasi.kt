3568764c6b4e19368cdfb7a0fd06cdd02530370d
agilimam <agilimamalhafizi04@gmail.com>
2024-12-22 10:51:49 +0700

    menambahkan alamt navigasi untuk detail barang

--- app/src/main/java/com/example/ucp2pam/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/ucp2pam/ui/navigation/AlamatNavigasi.kt
@@ -4,8 +4,26 @@ interface AlamatNavigasi{
     val route :String
 }
 
+object DestinasiPertama : AlamatNavigasi{
+    override val route ="Pertama"
+}
 
+object DestinasiHomeBrg : AlamatNavigasi{
+    override val route = "Home-Barang"
+}
 
 object DestinasiInsertBrg: AlamatNavigasi{
     override val route = "Insert-Brg"
+}
+
+object DestinasiDetailBarang : AlamatNavigasi{
+    override val route ="detail"
+    const val Nama = "Nama"
+    val  routesWithArg = "$route/{$Nama}"
+}
+
+object DestinasiUpdateBrg : AlamatNavigasi{
+    override val route = "update"
+    const val Nama = "nama"
+    val routesWithArg = "$route/{$Nama}"
 }
