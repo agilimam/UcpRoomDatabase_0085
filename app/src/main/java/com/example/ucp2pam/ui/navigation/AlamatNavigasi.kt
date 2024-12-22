6471e87bce2947d9df0307c6a5522b74a2925050
agilimam <agilimamalhafizi04@gmail.com>
2024-12-22 09:25:02 +0700

    membuat alamat navigasin untuk DestinasiInsertBrg

---
+++ app/src/main/java/com/example/ucp2pam/ui/navigation/AlamatNavigasi.kt
@@ -0,0 +1,11 @@
+package com.example.ucp2pam.ui.navigation
+
+interface AlamatNavigasi{
+    val route :String
+}
+
+
+
+object DestinasiInsertBrg: AlamatNavigasi{
+    override val route = "Insert-Brg"
+}
