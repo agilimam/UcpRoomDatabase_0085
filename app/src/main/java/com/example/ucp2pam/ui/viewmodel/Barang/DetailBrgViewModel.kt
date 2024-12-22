ea47f3c1fcc2f19cdd447731d5f0bd3cc64546b3
agilimam <agilimamalhafizi04@gmail.com>
2024-12-22 11:01:40 +0700

    membuat Data class untuk menampung data yang akan di tampilkan di UI

--- app/src/main/java/com/example/ucp2pam/ui/viewmodel/Barang/DetailBrgViewModel.kt
+++ app/src/main/java/com/example/ucp2pam/ui/viewmodel/Barang/DetailBrgViewModel.kt
@@ -2,8 +2,20 @@ package com.example.ucp2pam.ui.viewmodel.Barang
 
 import com.example.ucp2pam.data.entity.Barang
 
-// Memindahkan data dari entity ke ui
+data class DetailUiStateBrg(
+    val detailUiEventBrg:BarangEvent =BarangEvent(),
+    val isLoadingBrg: Boolean = false,
+    val isErrorBrg: Boolean = false,
+    val errorMessageBrg: String = ""
 
+) {
+    val isUiEventEmptyBrg: Boolean
+        get() = detailUiEventBrg == BarangEvent()
+    val isUiEvenNotEmptyBrg: Boolean
+        get() = detailUiEventBrg != BarangEvent()
+}
+
+// Memindahkan data dari entity ke ui
 fun Barang.toDetailUiEventBrg(): BarangEvent{
     return BarangEvent(
         id = id,
