10a98f28c8e42defd0282c245e80b10cd2072683
agilimam <agilimamalhafizi04@gmail.com>
2024-12-22 08:40:44 +0700

    menginisialisasi HomeBrgViewModel

--- app/src/main/java/com/example/ucp2pam/ui/view/Barang/HomeBrgView.kt
+++ app/src/main/java/com/example/ucp2pam/ui/view/Barang/HomeBrgView.kt
@@ -1,8 +1,10 @@
 package com.example.ucp2pam.ui.view.Barang
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -13,16 +15,90 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ucp2pam.data.entity.Barang
+import com.example.ucp2pam.ui.viewmodel.Barang.HomeBrgUiState
+import kotlinx.coroutines.launch
+
+@Composable
+fun HomeBrgView(
+
+){
+
+}
+@Composable
+fun BodyHomeBrgView(
+    homeBrgUiState: HomeBrgUiState,
+    onClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+){
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() } // Snackbar state
+    when {
+        homeBrgUiState.isLoadingBrg -> {
+            // Menampilkan indikator loading
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+
+        homeBrgUiState.isErrorBrg -> {
+            //Menampilkan pesarn error
+            LaunchedEffect(homeBrgUiState.errorMessageBrg) {
+                homeBrgUiState.errorMessageBrg?.let { message ->
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(message) //Tampilkan Snackbar
+                    }
+                }
+            }
+        }
+
+        homeBrgUiState.listBrg.isEmpty() -> {
+            // Menampilkan pesan jika data kosong
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Tidak ada data Barang.",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+
+        else -> {
+            //Menampilkan daftar Barang
+            ListBarang(
+                listBrg = homeBrgUiState.listBrg,
+                onClick = {
+                    onClick(it)
+                    println(
+                        it
+                    )
+                },
+                modifier = modifier
+            )
+        }
+    }
+}
 
 @Composable
 fun ListBarang(
