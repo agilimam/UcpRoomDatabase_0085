ffe071edd43f16102c918ed0b142405963ad42a9
agilimam <agilimamalhafizi04@gmail.com>
2024-12-22 08:58:42 +0700

    membuat  data class untuk FormErrorStateBrg

--- app/src/main/java/com/example/ucp2pam/ui/viewmodel/Barang/InsertBrgViewModel.kt
+++ app/src/main/java/com/example/ucp2pam/ui/viewmodel/Barang/InsertBrgViewModel.kt
@@ -2,6 +2,14 @@ package com.example.ucp2pam.ui.viewmodel.Barang
 
 import com.example.ucp2pam.data.entity.Barang
 
+data class FormErrorStateBrg(
+    val id: String? = null,
+    val Nama: String? =null,
+    val Deskripsi: String? =null,
+    val Harga: String? =null,
+    val Stok: String? =null,
+    val NamaSuplier: String? =null,
+)
 fun BarangEvent.toBarangEntity():Barang = Barang(
     id = id,
     Nama = Nama,
