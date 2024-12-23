d33e615a9c8131ae6980e5e9e2d895256bb453ab
agilimam <agilimamalhafizi04@gmail.com>
2024-12-24 02:01:07 +0700

    Menambahkan ViewModel untuk halaman utama dengan pengelolaan status dan penanganan error

--- app/src/main/java/com/example/ucp2pam/ui/viewmodel/Suplier/HomeSplViewModel.kt
+++ app/src/main/java/com/example/ucp2pam/ui/viewmodel/Suplier/HomeSplViewModel.kt
@@ -1,9 +1,54 @@
 package com.example.ucp2pam.ui.viewmodel.Suplier
 
-import com.example.ucp2pam.data.entity.Barang
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2pam.data.entity.Suplier
+import com.example.ucp2pam.repository.RepositorySuplier
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
-data class HomeUiState(
-    val listBrg: List<Barang> = listOf(),
+class HomeSplViewModel (
+    private val  repositorySuplier: RepositorySuplier
+
+) : ViewModel() {
+    val homeUIState : StateFlow<HomeUIState> = repositorySuplier.getAllSuplier()
+        .filterNotNull()
+        .map {
+            HomeUIState(
+                listSpl = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUIState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUIState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUIState(
+                isLoading = true,
+            )
+        )
+}
+
+data class HomeUIState(
+    val listSpl: List<Suplier> = listOf(),
     val isLoading: Boolean = false,
     val isError: Boolean = false,
     val errorMessage: String = ""
