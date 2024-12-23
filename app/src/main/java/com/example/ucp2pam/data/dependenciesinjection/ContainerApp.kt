f32eb7244c788f5554403ec4faf438e77fd5a109
agilimam <agilimamalhafizi04@gmail.com>
2024-12-24 01:54:56 +0700

    memperbaiki typo

--- app/src/main/java/com/example/ucp2pam/data/dependenciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/ucp2pam/data/dependenciesinjection/ContainerApp.kt
@@ -2,10 +2,10 @@ package com.example.ucp2pam.data.dependenciesinjection
 
 import android.content.Context
 import com.example.ucp2pam.data.database.AppDatabase
-import com.example.ucp2pam.data.repository.LocalRepositoryBarang
-import com.example.ucp2pam.data.repository.LocalRepositorySuplier
-import com.example.ucp2pam.data.repository.RepositoryBarang
-import com.example.ucp2pam.data.repository.RepositorySuplier
+import com.example.ucp2pam.repository.LocalRepositoryBarang
+import com.example.ucp2pam.repository.LocalRepositorySuplier
+import com.example.ucp2pam.repository.RepositoryBarang
+import com.example.ucp2pam.repository.RepositorySuplier
 
 interface InterfaceContainerApp {
     val repositoryBarang : RepositoryBarang
