c0486a46aee1cc56168b1106f545a4f10d7eabf2
agilimam <agilimamalhafizi04@gmail.com>
2024-12-24 01:53:42 +0700

    menambahkan alamat navigasi untuk home suplier

--- app/src/main/java/com/example/ucp2pam/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/ucp2pam/ui/navigation/AlamatNavigasi.kt
@@ -12,15 +12,17 @@ object DestinasiHomeBrg : AlamatNavigasi{
     override val route = "Home-Barang"
 }
 
-
+object DestinasiHomeSpl : AlamatNavigasi{
+    override val route = "Home-Suplier"
+}
 object DestinasiDetailBarang : AlamatNavigasi{
     override val route ="detail"
-    const val ID = "id"
+    const val ID = "id-detail"
     val  routesWithArg = "$route/{$ID}"
 }
 
 object DestinasiUpdateBrg : AlamatNavigasi{
     override val route = "update"
-    const val ID = "id"
+    const val ID = "id-update"
     val routesWithArg = "$route/{$ID}"
 }
