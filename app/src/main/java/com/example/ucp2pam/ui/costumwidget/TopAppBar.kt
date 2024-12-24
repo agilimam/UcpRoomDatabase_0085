f93a4744a26f724375955d76c94ef7492595c3b1
agilimam <agilimamalhafizi04@gmail.com>
2024-12-24 09:41:31 +0700

    menambahkan icon untuk button back

--- app/src/main/java/com/example/ucp2pam/ui/costumwidget/TopAppBar.kt
+++ app/src/main/java/com/example/ucp2pam/ui/costumwidget/TopAppBar.kt
@@ -2,56 +2,54 @@ package com.example.ucp2pam.ui.costumwidget
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-
+import com.example.ucp2pam.R
 
 
 @Composable
 fun TopAppBar(
     showBackButton: Boolean = true,
     judul: String,
-    onBack : () -> Unit,
+    onBack: () -> Unit,
     modifier: Modifier = Modifier
-    ) {
+) {
     Box(
         modifier = Modifier
             .fillMaxWidth()
             .background(Color(0XFF09f9d2))
-            .padding(16.dp),
-        contentAlignment = Alignment.Center // Pastikan konten di tengah
+            .padding(horizontal = 16.dp, vertical = 15.dp),
+        contentAlignment = Alignment.Center
     ) {
         if (showBackButton) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
+            IconButton(
+                onClick = onBack,
+                modifier = Modifier.align(Alignment.CenterStart)
             ) {
-                TextButton(
-                    onClick = onBack,
-                    modifier = Modifier.align(Alignment.CenterVertically)
-                ) {
-                    Text("Kembali")
-                }
-                Spacer(modifier = Modifier.weight(2f))
+                Icon(
+                    painter = painterResource(id = R.drawable.back),
+                    contentDescription = "Kembali",
+                    tint = Color.Black
+                )
             }
         }
 
-        // Teks judul
         Text(
             text = judul,
-            fontSize = 25.sp,
+            fontSize = 20.sp,
             fontWeight = FontWeight.Bold,
+            color = Color.Black,
             modifier = Modifier.align(Alignment.Center)
         )
     }
