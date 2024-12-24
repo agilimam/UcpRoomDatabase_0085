74e331178f08cb775aac20a14e7c5362a2fef894
agilimam <agilimamalhafizi04@gmail.com>
2024-12-24 12:27:15 +0700

    menambahkan desain tampilan halaman utama

--- app/src/main/java/com/example/ucp2pam/ui/view/PilihHomeView.kt
+++ app/src/main/java/com/example/ucp2pam/ui/view/PilihHomeView.kt
@@ -1,58 +1,189 @@
 package com.example.ucp2pam.ui.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-
-
+import com.example.ucp2pam.R
 @Composable
 fun PilihanHomeView(
     onAddBrgClick: () -> Unit,
-    onListBrgClick: () ->Unit,
+    onListBrgClick: () -> Unit,
+    onAddSplClick: () -> Unit,
+    onListSplClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Box(
-        modifier = modifier
+    Column(
+        modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
-        contentAlignment = Alignment.Center
+            .background(Color(0xFFF5F5F5)),
+        verticalArrangement = Arrangement.Top
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(250.dp)
+                .clip(RoundedCornerShape(20.dp)) // Pastikan clip di sini
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(Color(0xFF09f9d2), Color(0xFF0dd49c))
+                    )
+                )
         ) {
-            Text(
-                text = "Pilih Halaman",
-                style = MaterialTheme.typography.headlineMedium,
-                modifier = Modifier.padding(bottom = 32.dp)
+            Image(
+                painter = painterResource(id = R.drawable.baner),
+                contentDescription = "Header Image",
+                modifier = Modifier
+                    .size(180.dp)
+                    .clip(CircleShape)
+                    .align(Alignment.Center)
+                    .padding(16.dp),
+                contentScale = ContentScale.Crop
             )
 
-            // Tombol untuk AddBrg
-            Button(
-                onClick = onAddBrgClick,
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 16.dp)
+                    .align(Alignment.TopStart)
+                    .padding(16.dp)
             ) {
-                Text(text = "Add Barang")
+                Icon(
+                    painter = painterResource(id = R.drawable.homeicon),
+                    contentDescription = "Home Icon",
+                    modifier = Modifier
+                        .size(72.dp)
+                        .clip(CircleShape)
+                        .background(Color.White)
+                        .padding(8.dp),
+                    tint = Color.Unspecified
+                )
+                Text(
+                    text = "Inventory",
+                    style = MaterialTheme.typography.headlineSmall,
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(top = 8.dp)
+                )
             }
+        }
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.CenterHorizontally)
+        ){
+            Text(
+                text = "Mari Kita Coba ",
+                style = MaterialTheme.typography.headlineSmall,
+                color = Color.Black,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(top = 8.dp)
 
-            Button(
-                onClick = onListBrgClick,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 16.dp)
-            ) {
-                Text(text = "ListBrg")
+            )
+
+        }
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            contentPadding = PaddingValues(8.dp)
+        ) {
+            item {
+                MenuItemCard(
+                    title = "Add Barang",
+                    icon = R.drawable.addbrg,
+                    onClick = onAddBrgClick
+                )
+            }
+            item {
+                MenuItemCard(
+                    title = "List Barang",
+                    icon = R.drawable.listbrg,
+                    onClick = onListBrgClick
+                )
+            }
+            item {
+                MenuItemCard(
+                    title = "Add Supplier",
+                    icon = R.drawable.addspl,
+                    onClick = onAddSplClick
+                )
+            }
+            item {
+                MenuItemCard(
+                    title = "List Supplier",
+                    icon = R.drawable.listspl,
+                    onClick = onListSplClick
+                )
             }
         }
     }
 }
+
+@Composable
+fun MenuItemCard(
+    title: String,
+    icon: Int,
+    onClick: () -> Unit
+) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .clickable(onClick = onClick),
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Icon(
+                painter = painterResource(id = icon),
+                contentDescription = title,
+                modifier = Modifier
+                    .size(56.dp)
+                    .padding(bottom = 8.dp),
+                tint = Color.Unspecified
+            )
+
+
+            Text(
+                text = title,
+                style = MaterialTheme.typography.bodySmall,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
+
